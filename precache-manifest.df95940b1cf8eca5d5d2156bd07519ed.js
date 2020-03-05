61607bc2eb711b17a6e67b1fba1b3c478498b837
R <R@MacBook-Air-Micha.local>
2020-03-05 12:28:50 +0100

    Updates

---
+++ precache-manifest.df95940b1cf8eca5d5d2156bd07519ed.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4a74e17efead18ef2a5a534819de01",
+    "url": "/CarbonFootprint/index.html"
+  },
+  {
+    "revision": "fda305400072fd1327e0",
+    "url": "/CarbonFootprint/static/css/main.2d08369e.chunk.css"
+  },
+  {
+    "revision": "a52807d61fedc395865f",
+    "url": "/CarbonFootprint/static/js/2.a88ededc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/CarbonFootprint/static/js/2.a88ededc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda305400072fd1327e0",
+    "url": "/CarbonFootprint/static/js/main.e0f101b0.chunk.js"
+  },
+  {
+    "revision": "6fb63bc838720bfe5e15",
+    "url": "/CarbonFootprint/static/js/runtime-main.66971f4a.js"
+  },
+  {
+    "revision": "5963b991b3daa5839cfc6a5af9f72be8",
+    "url": "/CarbonFootprint/static/media/def.5963b991.png"
+  },
+  {
+    "revision": "06c8751fe953db9b3dac107e49a59edc",
+    "url": "/CarbonFootprint/static/media/fontello.06c8751f.woff"
+  },
+  {
+    "revision": "464862312358dae6038eac55643fdd46",
+    "url": "/CarbonFootprint/static/media/fontello.46486231.ttf"
+  },
+  {
+    "revision": "8e45fc987259a65beb47c1714e7a5ec8",
+    "url": "/CarbonFootprint/static/media/fontello.8e45fc98.svg"
+  },
+  {
+    "revision": "a91a0a394b0370a38551f87c512ce1a6",
+    "url": "/CarbonFootprint/static/media/fontello.a91a0a39.woff2"
+  },
+  {
+    "revision": "dae408b04b3ea5d65aa8b3d1ce7e1712",
+    "url": "/CarbonFootprint/static/media/fontello.dae408b0.eot"
+  },
+  {
+    "revision": "5f548887dec7d73eed1f4bb161040989",
+    "url": "/CarbonFootprint/static/media/foot.5f548887.png"
+  },
+  {
+    "revision": "c4ca4b9d0a2893921b2fa29fcea4d3d8",
+    "url": "/CarbonFootprint/static/media/kyle-glenn.c4ca4b9d.jpg"
+  },
+  {
+    "revision": "3e181954c68c22ad650eb78c09dc9364",
+    "url": "/CarbonFootprint/static/media/larm-rmah.3e181954.jpg"
+  },
+  {
+    "revision": "be9f04f50fca288090d923f442d1f9d6",
+    "url": "/CarbonFootprint/static/media/reduce.be9f04f5.jpeg"
+  }
+]);
