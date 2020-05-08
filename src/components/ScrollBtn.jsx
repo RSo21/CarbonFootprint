c7573eb74f4004d963502958128186db8ec4441c
R <roksana_sobczak@wp.pl>
2020-05-08 17:58:28 +0200

    change scroll up btn size

--- src/components/ScrollBtn.jsx
+++ src/components/ScrollBtn.jsx
@@ -2,12 +2,39 @@ import React, { Component } from 'react';
 import ScrollUpButton from "react-scroll-up-button";
 
  class ScrollBtn extends Component {
+    state = {
+        width: window.innerWidth,
+    };
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
     render() {
-        return (
-            <div>
-                <ScrollUpButton style={{backgroundColor: "#57B850"}}/>
-            </div>
-        );
+        const { width } = this.state;
+        const isMobile = width <= 978;
+
+        if(isMobile){
+            return(
+                <div>
+                    <ScrollUpButton style={{backgroundColor: "#57B850", width: '2.3em', height: '2.3em'}} />
+                </div>
+            )
+
+        }else{
+            return (
+                <div>
+                    <ScrollUpButton style={{backgroundColor: "#57B850"}}/>
+                </div>
+            );
+        }
     }
 }
 
